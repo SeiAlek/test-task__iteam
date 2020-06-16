3304834947163a6f34985a24415aedd4756b17b2
Oleksii Mykhailov <alem.coding@gmail.com>
2020-06-16 20:59:59 +0300

    Solution beta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tt-iteam';
+  title = 'Timer';
+  timerData = 0;
+  isStarted = false;
+
+  btnText: 'Start' | 'Pause' | 'Continue' = 'Start';
+  timer;
+
+  handleStart() {
+    if (this.isStarted) {
+      this.timer.unsubscribe();
+      this.btnText = 'Continue';
+      this.isStarted = false;
+      return;
+    }
+
+    this.timer = interval(1000).subscribe(value => {
+      this.timerData += 1000;
+      console.log(value);
+    });
+
+    this.isStarted = true;
+    this.btnText = 'Pause';
+  }
+
+  handlePause() {
+    if (!this.isStarted) {
+      return;
+    }
+
+    this.btnText = 'Continue';
+    this.isStarted = false;
+    this.timer.unsubscribe();
+  }
+
+  handleReset() {
+    if (!this.timer) {
+      return;
+    }
+
+    this.timerData = 0;
+    this.isStarted = false;
+    this.btnText = 'Start';
+    this.timer.unsubscribe();
+  }
 }
+
